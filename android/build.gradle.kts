c7b3eec8c308ef0049032e4165cbff8dbc6d054b
OMR <cartoon6676@gmail.com>
2025-04-24 21:51:57 +0200

    first commit

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// build.gradle (Project level)
+
+buildscript {
+    repositories {
+        google()  // Required for Google services plugin
+        mavenCentral()  // Required for maven dependencies
+    }
+    dependencies {
+        classpath "com.google.gms:google-services:4.4.2"  // version "4.4.2"
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +17,7 @@ allprojects {
     }
 }
 
+// Custom Build Directory Setup
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,10 +25,13 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+// Ensure the :app project is evaluated first
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Custom Clean Task
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
